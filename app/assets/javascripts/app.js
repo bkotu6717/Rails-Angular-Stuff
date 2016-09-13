2c1c2e61dac16064ab0df280e95bac16f9933de2
robin <robinthomas@railsfactory.org>
2016-09-13 19:32:33 +0530

    xedit, angular inline edit completed

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,7 @@
-app = angular.module('angularRailsStuff', ['ngRoute', 'templates', 'bw.paging', '720kb.datepicker']);
-
+app = angular.module('angularRailsStuff', ['ngRoute', 'templates', 'bw.paging', '720kb.datepicker','xeditable']);
+app.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
     .when("/", {

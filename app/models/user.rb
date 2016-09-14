73e46cae2354efc87d4385ab4acd1639f5f809b3
railsfactory-bhaskararao <bhaskararao@railsfactory.org>
2016-09-14 18:21:33 +0530

    remove elastic search

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-	include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
 	# validates_format_of :name, with: /\A[a-z]([-']?[a-z]+)*( [a-z]([-']?[a-z]+)*)+\z/
 	validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, uniqueness: true
 	validates_format_of :dob, with: /\d{4}-\d{2}-\d{2}/
